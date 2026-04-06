41607c9e939d4fa514426b4b461d49ea92fbf885
martinasergi <msergiirazoqui@udesa.edu.ar>
2026-04-06 20:32:04 -0300

    Update validacion_datos.py

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -1,7 +1,63 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 27 17:37:44 2026
+def validar_registro(registro, claves):
+    '''
+    verifica que cada diccionario dentro de 'claves':
+    
+    1. Exista dentro de 'registro'
+    2. Contenga los campos obligatorios
+    3. Que cada campo tenga el tipo correcto:
+    
+    - "id_participante" → int
+    - "tiempo" → str
+    - "valor" → str
+    - "fase" → str
+    - "condicion_experimental" → str
+    - "hit" → str
 
-@author: Usuario
-"""
+    Return:
+        True si todo es válido
+        False si alguna condición no se cumple
+    '''
 
+    for clave in claves:
+        if clave not in registro:
+            return False
+
+    for diccionario in claves:
+
+        id_participante = diccionario["id_participante"]
+        try:
+            id_participante + 0
+        except:
+            return False
+
+        tiempo = diccionario["tiempo"]
+        try:
+            tiempo + ""
+        except:
+            return False
+
+        valor = diccionario["valor"]
+        try:
+            valor + ""
+        except:
+            return False
+
+        fase = diccionario["fase"]
+        try:
+            fase + ""
+        except:
+            return False
+
+        condicion = diccionario["condicion_experimental"]
+        try:
+            condicion + ""
+        except:
+            return False
+
+        hit = diccionario["hit"]
+        try:
+            hit + ""
+        except:
+            return False
+
+    return True
