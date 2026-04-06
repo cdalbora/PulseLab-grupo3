cd636a65daf2b1b002453f92e7c6ead421149523
victoriagaal <victoria.gaal1@gmail.com>
2026-04-06 20:14:29 -0300

    procesamiento_datos
    procesamiento_datos anadido

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -1,7 +1,39 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 27 17:38:43 2026
 
-@author: Usuario
-"""
+def filtrar_datos(datos):
+    '''
+    Prepara los datos de PulseLab para el análisis, reuniendo
+    los valores de la señal, los tiempos y la cantidad de hits.
 
+    Parameters
+    ----------
+    datos : lista
+
+        lista de diccionarios con registros validados del experimento.
+
+    Returns
+    -------
+    dict
+        - diccionario con:
+            * "valor": lista de valores de la señal
+            * "tiempo": lista de tiempos
+            * "hit": cantidad de hits detectados
+            * "total_registros": cantidad total de registros.
+
+    '''
+    valores = []
+    tiempos = []
+    hits = 0
+
+    for registro in datos:
+        valores.append(registro["valor"])
+        tiempos.append(registro["tiempo"])
+
+        if registro["hit"] == 1 or registro["hit"] == "1" or registro["hit"] == True:
+            hits += 1
+
+    return {
+        "valor": valores,
+        "tiempo": tiempos,
+        "hit": hits,
+        "total_registros": len(datos)
+    }
